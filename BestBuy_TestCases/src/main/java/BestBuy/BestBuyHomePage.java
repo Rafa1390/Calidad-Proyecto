244c7a7a4e88753df9d451e456b919f9f2bf7bf6
Carlos Alvarado <calvaradon@ucenfotec.ac.cr>
2021-04-08 22:04:09 -0600

    Se cambió el archivo test suite y se completó la prueba CP-004
    Signed-off-by: Carlos Alvarado <calvaradon@ucenfotec.ac.cr>

--- BestBuy_TestCases/src/main/java/BestBuy/BestBuyHomePage.java
+++ BestBuy_TestCases/src/main/java/BestBuy/BestBuyHomePage.java
@@ -3,10 +3,15 @@ package BestBuy;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.util.HashMap;
+
 public class BestBuyHomePage extends BasePage{
     //elements paths
     By Logo_BestBuy = By.xpath("//img[@class=\"logo\"]");
     By Close_Email_Modal = By.xpath("//button[@class=\"c-close-icon  c-modal-close-icon\"]");
+    By Account_Menu = By.xpath("//*[@id=\"header-block\"]/div[2]/div[2]/div/nav[2]/ul/li[1]/button/div[2]");
+    By Create_Account_Btn = By.xpath("//*[@id=\"ABT2465Menu\"]/header/a[2]");
+    By Submit_New_User = By.xpath("/html/body/div[1]/div/section/main/div[2]/div[1]/div/div/div/div/form/div[9]/button");
 
     BestBuyHomePage(WebDriver driver) {
         super(driver);
@@ -14,5 +19,26 @@ public class BestBuyHomePage extends BasePage{
 
     public void clickBestBuy(){
         click(Close_Email_Modal);
+    }
+
+    public void openAccountMenu() {
+        click(Account_Menu);
+    }
+
+    public void clickCreateAccount() {
+        click(Create_Account_Btn);
+    }
+
+    public void fillCreateAccountForm(HashMap<String, String> userData ){
+        sendKeys(By.name("firstName"), userData.get("firt_name"));
+        sendKeys(By.name("lastName"), userData.get("last_name"));
+        sendKeys(By.name("email"), userData.get("email"));
+        sendKeys(By.name("fld-p1"), userData.get("password"));
+        sendKeys(By.name("reenterPassword"), userData.get("password"));
+        sendKeys(By.name("phone"), userData.get("phone_number"));
+    }
+
+    public void createAccount() {
+        click(Submit_New_User);
     }
 }
---
+++ BestBuy_TestCases/src/test/java/BestBuy/TestCreateAccount.java
@@ -0,0 +1,32 @@
+package BestBuy;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+public class TestCreateAccount extends BaseTest{
+
+    @Parameters({"first-name", "last-name", "email", "password", "phone-number"})
+    @Test
+    public void createAccountTest(String firstName, String lastName, String email, String password, String phone) throws InterruptedException {
+        BBHP.clickBestBuy();
+        BBHP.openAccountMenu();
+        BBHP.clickCreateAccount();
+        HashMap<String, String> userInfo = mapUserData(firstName,lastName,email,password,phone);
+        BBHP.fillCreateAccountForm(userInfo);
+        BBHP.createAccount();
+        Thread.sleep(8000);
+    }
+
+    private HashMap<String, String> mapUserData(String firstName, String lastName, String email, String password, String phone) {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("firt_name", firstName);
+        data.put("last_name", lastName);
+        data.put("email", email);
+        data.put("password", password);
+        data.put("phone_number", phone);
+        return data;
+    }
+}
