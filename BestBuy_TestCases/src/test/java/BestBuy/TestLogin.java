1a50dd63c1a745d128c78861149b96c76cc19f61
Elmer Moraga <moragaelmer09@gmail.com>
2021-04-29 22:19:50 -0600

    Se reutiliza el test login que tambien validad CP-012

--- BestBuy_TestCases/src/test/java/BestBuy/TestLogin.java
+++ BestBuy_TestCases/src/test/java/BestBuy/TestLogin.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 
 public class TestLogin extends BaseTest{
 
+    //Es test tambien valida el test CP-012
     @Parameters({"email", "password"})
     @Test
     public void login(String email, String password){
